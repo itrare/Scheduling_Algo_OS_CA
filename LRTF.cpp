45b23d0b80d8096d1e25b8c6b8aa3abba8d3b492
Abhishek Kumar Gupta <38077557+itrare@users.noreply.github.com>
2019-04-09 19:03:58 +0530

    Update LRTF.cpp

--- LRTF.cpp
+++ LRTF.cpp
@@ -1,4 +1,14 @@
 // LRTF.cpp
+/*
+
+Three students (a, b, c) are arriving in the mess at the same time. The id numbers of
+these students are 2132, 2102, 2453 and the food taken time from the mess table is 2, 4 and 8
+minutes. If the two students have same remaining time so it is broken by giving priority to the
+students with the lowest id number. Consider the longest remaining time first (LRTF) scheduling
+algorithm and calculate the average turnaround time and waiting time.
+
+
+*/
 #include<iostream>
 #include<stdlib.h>
 #include<conio.h>
@@ -8,24 +18,21 @@ COORD coord = {0, 0};
 COORD max_res,cursor_size;
 void gotoxy (int x, int y)
 {
-     coord.X = x; coord.Y = y; // X and Y coordinates
-     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+        coord.X = x; coord.Y = y; // X and Y coordinates
+        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
+
 
 using namespace std;
 int cc;
 struct process{
  int PID,at,bt,btt,wt,ct,tat,PR;
 };
+
 process *ProcessQueue = new process[MAX];
-/*
-Three students (a, b, c) are arriving in the mess at the same time. The id numbers of
-these students are 2132, 2102, 2453 and the food taken time from the mess table is 2, 4 and 8
-minutes. If the two students have same remaining time so it is broken by giving priority to the
-students with the lowest id number. Consider the longest remaining time first (LRTF) scheduling
-algorithm and calculate the average turnaround time and waiting time.
-*/
+
 int sizeBTQueue(process *Array[]){
+
     int isThere=0;
     for(int i=0;i<cc;i++){
         if(Array[i]->bt>0){
@@ -36,6 +43,7 @@ int sizeBTQueue(process *Array[]){
     return isThere;
 }
 int isNullQueue(process *Array[]){
+
     int isThere=0;
     for(int i=0;i<cc;i++){
 
@@ -43,6 +51,7 @@ int isNullQueue(process *Array[]){
             isThere=1;
         }
     }
+
     return isThere;
 }
 int allDone(){
@@ -53,20 +62,29 @@ int allDone(){
             isThere=1;
         }
     }
+
     return isThere;
+
 }
 int x=1,m=1,d;
 void madeGanttChar(int id[],int ct[],int z){
+
+
 int i=0;
 d=0;
 ct[0]=0;
+
 for( m =1,x=1;m<=z;m++,x++){
+
 if(m%15==0){
+
     d+=5;
     x=1;
 
     gotoxy(x*7,d+2);
 }
+
+
     gotoxy(x*7,1+d);
     cout<<" ______";
     gotoxy(x*7,1+d);
@@ -78,10 +96,13 @@ if(m%15==0){
     cout<<"|"<<endl;
     gotoxy(x*7,3+d);
     cout<<"|______|"<<endl;
+
+
 }
 
 gotoxy(x*7,1+d);
 cout<<""<<ct[m-1]<<endl;
+
 
 }
 process &priorityScheduling(process *longAssump, process *CompV){
@@ -123,8 +144,6 @@ void lRTFScheduling(){
         longRProc->tat = longRProc->ct - longRProc->at;
         longRProc->wt = longRProc->tat - longRProc->btt;
 
-        cout<<"Long ID:"<<longRProc->PID<<"Long Prcs bt:- and ct:-"<<longRProc->bt<<" " <<longRProc->ct<<endl;
-
 
     system("cls");
     id[i]=longRProc->PID;
@@ -165,18 +184,25 @@ int main(){
     display();
 
 }
-/*
 
+/*
+Test Case #Original:
+3
 2132 0 2
 2102 0 4
 2453 0 8
 
+Test Case #1:
+3
 2132 0 2
 2102 2 4
 2453 4 8
 
+Test Case #2:
+3
 1 4 32
 2 5 57
 3 5 47
 
 */
+
